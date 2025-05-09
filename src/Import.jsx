629b393d9510d994dfb7c01e13e3b91a83d4b9b7
ASHIK1108 <ashikbarman2002@gmail.com>
2025-05-10 02:05:40 +0530

    home page componets add

--- src/Import.jsx
+++ src/Import.jsx
@@ -1,2 +1,6 @@
 export {default as Header} from '@/components/Header';
 export {default as Home} from '@/pages/Home';
+export {default as Services} from '@/components/Services';
+export {default as OurArea} from '@/components/OurArea';
+export {default as Hero} from '@/components/Hero';
+export {default as LatestVideosImages} from '@/components/LatestVideosImages';
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
-import Hero from "../components/Hero";
+import {Hero, Services, OurArea, LatestVideosImages} from '@/Import';
 
 export default function Home(){
     return(
         <>
         <Hero/>
-        
+        <Services/>
+        <OurArea/>
+        <LatestVideosImages/>
         </>
     )
 }
