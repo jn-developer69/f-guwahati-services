53bc435b5795cad3fc086d6377fe889acfa1ab72
ASHIK1108 <ashikbarman2002@gmail.com>
2025-05-09 23:50:02 +0530

    Added Header 69

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import InterlockingApplianceRepairHeader from './components/header'
+import PremiumServicesHeader from './components/header'
 // import EcommerceApp from './EcommerceApp'
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <>
-      <InterlockingApplianceRepairHeader /> 
+      <PremiumServicesHeader /> 
     </>
   )
 }
