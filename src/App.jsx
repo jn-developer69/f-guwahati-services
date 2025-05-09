ae96ad8c6544863580013b552006a3d418f628f0
ASHIK1108 <ashikbarman2002@gmail.com>
2025-05-10 00:35:57 +0530

    updated import

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import PremiumServicesHeader from './components/header'
-// import EcommerceApp from './EcommerceApp'
+import {Header, Home} from '@/Import';
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <PremiumServicesHeader /> 
+      <Header /> 
+      <Home/>
     </>
   )
 }
